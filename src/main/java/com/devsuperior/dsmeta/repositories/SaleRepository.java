07679b134b4e36a88d7789f27f548614fb575fac
Renata Viotto Hassum <renatahassum@gmail.com>
2022-10-11 16:40:15 -0300

    Report | Repository - SaleReportProjection - SaleReportDTO

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,17 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.projections.SaleReportProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    List<SaleReportProjection> searchReport(LocalDate minDate, LocalDate maxDate, String name);
+
+
 
 }
