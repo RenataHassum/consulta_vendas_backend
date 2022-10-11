082adcf6e4d29819d2e964971351b050d5c2ebe5
Renata Viotto Hassum <renatahassum@gmail.com>
2022-10-11 17:08:28 -0300

    Report | Service - Controller - SQL Repository

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -4,12 +4,18 @@ import com.devsuperior.dsmeta.projections.SaleReportProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query(nativeQuery = true, value = "SELECT tb_sales.id, tb_sales.date, tb_sales.amount, tb_seller.name AS sellerName, SUM(tb_sales.amount) FROM tb_seller " +
+            "INNER JOIN tb_sales ON tb_sales.seller_id = tb_seller.id " +
+            "WHERE UPPER(tb_seller.name) LIKE UPPER(CONCAT('%', :name ,'%')) AND tb_sales.date BETWEEN :minDate AND :maxDate " +
+            "GROUP BY tb_sales.id, tb_sales.date,  tb_sales.amount, tb_seller.name " +
+            "ORDER BY tb_sales.date DESC")
     List<SaleReportProjection> searchReport(LocalDate minDate, LocalDate maxDate, String name);
 
 
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,7 +1,14 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.projections.SaleReportProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +28,17 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public List<SaleReportDTO> getReport(String minDate, String maxDate, String name) {
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate overAYear = today.minusYears(1L);
+
+		LocalDate min = "".equals(minDate) ? overAYear : LocalDate.parse(minDate);
+		LocalDate max = "".equals(maxDate) ? today : LocalDate.parse(maxDate);
+
+		List<SaleReportProjection> result = repository.searchReport(min, max, name);
+		return result.stream().map(x -> new SaleReportDTO(x)).collect(Collectors.toList());
 	}
 }
